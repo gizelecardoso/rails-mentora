6bac813a0bf134906d972acccf409ae825122cfb
Gizele Santos <gizele.ccardoso@gmail.com>
2024-11-09 18:56:37 -0300

    Criação de mentora (new e create)

--- app/controllers/mentoras_controller.rb
+++ app/controllers/mentoras_controller.rb
@@ -6,4 +6,20 @@ class MentorasController < ApplicationController
   def show
     @mentora = Mentora.find(params[:id])
   end
+
+  def new
+    @mentora = Mentora.new
+  end
+
+  def create
+    @mentora = Mentora.new(mentora_params)
+    @mentora.save
+    redirect_to @mentora
+  end
+
+  private
+
+  def mentora_params
+    params.require(:mentora).permit(:nome, :contato, :habilidades)
+  end
 end
